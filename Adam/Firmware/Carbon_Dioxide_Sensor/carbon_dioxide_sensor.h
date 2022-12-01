0486faba99c9299b4bf7aa08c373e8622735eea0
adampameron <82424820+adampameron@users.noreply.github.com>
2022-12-01 10:43:08 -0600

    Add files via upload

---
+++ Adam/Firmware/Carbon_Dioxide_Sensor/carbon_dioxide_sensor.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   carbon_dioxide_sensor.h
+ * Author: Adam Pameron
+ *
+ * Created on October 24, 2022, 3:00 PM
+ */
+
+#ifndef CARBON_DIOXIDE_SENSOR_H
+#define	CARBON_DIOXIDE_SENSOR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+float getCarbonDioxideData(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CARBON_DIOXIDE_SENSOR_H */
+
